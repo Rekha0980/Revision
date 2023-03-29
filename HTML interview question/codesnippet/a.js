116728d7b51b3f16abefa28f55b576fd58263ed9
rekhayadav <rekha.ee026@gmail.com>
2023-03-29 16:40:16 +0530

    code added

---
+++ HTML interview question/codesnippet/a.js
@@ -0,0 +1,33 @@
+// question 1
+// const a={}
+// const b={key:"b"}
+// const c={key:"c"}
+// a[b]=123;
+// a[c]=456
+// console.log(a[b])
+
+
+//question 2
+// function addToList(item,list){
+//     return list.push(item)
+// }
+
+// const res=addToList("apple",1)
+// console.log(res)
+
+// question 3
+
+// let x=1;
+// function foo(){
+//     x=2;
+//     console.log(x)
+// }
+// setTimeout(foo,0)
+// x=3;
+// console.log(x)
+
+//question 4
+
+// for(let i=0;i<3;i++){
+//     setTimeout(()=>{console.log(i)},1)
+// }
